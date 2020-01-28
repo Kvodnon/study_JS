4f0909ee073dc81e902d478497e19bb1314afb43
Kirill Kiselev <skulpin@mail.ru>
2020-01-29 00:11:39 +0700

    Второй урок

--- lesson01/script.js
+++ lesson01/script.js
@@ -1,10 +1,21 @@
-var money,
-  income,
-  addExpenses,
-  deposit,
-  mission,
-  period;
+let money = 10000,
+    income = "преподавание",
+    addExpenses = "Тренажерный зал, Спортивное питание, Досуг",
+    deposit = false,
+    mission = 60000,
+    period = 12;
+    
+console.log('money: ', typeof money);
+console.log('income: ', typeof income);
+console.log('deposit: ', typeof deposit);
 
-alert("Hello world по классике");
+console.log('addExpenses: ', addExpenses.length);
 
-console.log("Я на курсе JavaScript 11.0");
+console.log('Период равен ' + period + ' месяцев');
+console.log('Цель заработать ' + mission + ' рублей');
+
+console.log(addExpenses.toLowerCase().split(', '));
+
+let budgetDay = money / 30;
+
+console.log('budgetDay: ', budgetDay);
