6fd5c8259804d9a193a6b372fd9a247913730815
Kirill Kiselev <skulpin@mail.ru>
2020-02-12 12:12:36 +0700

    changes

--- script/script.js
+++ script/script.js
@@ -1,36 +1,78 @@
-'use strict';
+document.addEventListener("DOMContentLoaded", function() {
+  'use strict';
 
-const body = document.querySelector('body');
-
-const DomElement = function(selector, height, width, bg, fontSize) {
-  this.selector = selector;
-  this.height = height;
-  this.width = width;
-  this.bg = bg;
-  this.fontSize = fontSize;
-};
-
-DomElement.prototype.create = function() {
-  if (this.selector) {
-    if (this.selector.indexOf('.', 0) === 0) {
-      let element = document.createElement('div');
-      element.classList.add(this.selector.slice('1', this.selector.length));
-      body.append(element);
-      this.element = element;
-    }
-    
-    if (this.selector.indexOf('#', 0) === 0) {
-      let element = document.createElement('div');
-      element.id = this.selector.slice('1', this.selector.length);
-      body.append(element);
-      this.element = element;
-    }
-  }
-};
-
-DomElement.prototype.insertText = function(text) {
+  const body = document.querySelector('body');
   
-};
+  const DomElement = function(selector, height, width, bg, fontSize) {
+    this.selector = selector;
+    this.height = height;
+    this.width = width;
+    this.bg = bg;
+    this.fontSize = fontSize;
+  };
+  
+  DomElement.prototype.create = function() {
+    let element = document.createElement('div');
+  
+    if (this.selector) {
+      if (this.selector.indexOf('.', 0) === 0) {
+        element.classList.add(this.selector.slice('1', this.selector.length));
+      }
+      
+      if (this.selector.indexOf('#', 0) === 0) {
+        element.id = this.selector.slice('1', this.selector.length);
+      }
+    }
+  
+    element.style.height = this.height;
+    element.style.width = this.width;
+    element.style.background = this.bg;
+    element.style.fontSize = this.fontSize;
+  
+    body.append(element);
+    this.element = element;
+  };
+  
+  DomElement.prototype.insertText = function(text) {
+    this.element.innerText = text;
+  };
+  
+  let square = new DomElement('.square', '100px', '100px', 'yellow');
+  square.create();
+  square.insertText('test');
+  square.element.style.position = 'absolute';
 
-let square = new DomElement('.#lesson');
-square.create();
+  const keyCodes = [37,38,39,40],
+    codes = {
+      37: {
+        position: -10,
+        positionName: 'left'
+      },
+      38: {
+        position: -10,
+        positionName: 'top'
+      },
+      39: {
+        position: 10,
+        positionName: 'left'
+      },
+      40: {
+        position: 10,
+        positionName: 'top'
+      },
+    };
+
+  document.addEventListener("keydown", function(event) {
+    if (keyCodes.indexOf(event.keyCode) >= 0) {
+      let position = 0;
+
+      if (square.element.style[codes[event.keyCode].positionName]) {
+        position = parseInt(square.element.style[codes[event.keyCode].positionName]);
+      }
+
+      position += codes[event.keyCode].position;
+
+      square.element.style[codes[event.keyCode].positionName] = position + 'px';
+    }
+  });
+});
