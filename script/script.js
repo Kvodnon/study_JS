15fd1f67c5e5041c3168110ec84f006f3f0ff522
Kirill Kiselev <skulpin@mail.ru>
2020-02-21 01:35:46 +0700

    next stage

--- script/script.js
+++ script/script.js
@@ -5,7 +5,25 @@ window.addEventListener('DOMContentLoaded', () => {
     timerMinutes = document.getElementById('timer-minutes'),
     timerSeconds = document.getElementById('timer-seconds');
 
+  function addZero(number) {
+    number += '';
+
+    if (number.length === 1) {
+      number = '0' + number;
+    }
+
+    return number;
+  }
+
   function countTimer(deadline) {
+    function checkRemaining(timer) {
+      if (timer.timeRemaining <= 0) {
+        return { timeRemaining: timer.timeRemaining, hours: 0, minutes: 0, seconds: 0 };
+      }
+
+      return timer;
+    }
+
     function getTimeRemaining() {
       const dateStop = new Date(deadline).getTime(),
         dateNow = new Date().getTime(),
@@ -14,24 +32,21 @@ window.addEventListener('DOMContentLoaded', () => {
         minutes = Math.floor((timeRemaining / 60) % 60),
         hours = Math.floor(timeRemaining / 60 / 60);
 
-      return {timeRemaining, hours, minutes, seconds};
+      return checkRemaining({ timeRemaining, hours, minutes, seconds });
     }
+
 
     function updateClock() {
       const timer = getTimeRemaining();
-  
-      timerHours.textContent = timer.hours;
-      timerMinutes.textContent = timer.minutes;
-      timerSeconds.textContent = timer.seconds;
 
-      // if (timer.timeRemaining > 0) {
-      //   setTimeout(updateClock, 1000);
-      // }
+      timerHours.textContent = addZero(timer.hours);
+      timerMinutes.textContent = addZero(timer.minutes);
+      timerSeconds.textContent = addZero(timer.seconds);
     }
 
     updateClock();
   }
 
   // countTimer('23 february 2020');
-  setInterval(countTimer, 1000, '23 february 2020');
+  setInterval(countTimer, 1000, '21 february 2020');
 });
